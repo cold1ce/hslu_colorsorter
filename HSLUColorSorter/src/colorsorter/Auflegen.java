c8d102313a5d0239fb8e09b221e0d19e6c6d03a7
Julian <Julian@172.16.22.106>
2018-05-14 16:03:23 +0200

    Unwichtiges Aussortiert, und kommentiert.

--- HSLUColorSorter/src/colorsorter/Auflegen.java
+++ HSLUColorSorter/src/colorsorter/Auflegen.java
@@ -7,22 +7,26 @@ import lejos.hardware.port.MotorPort;
 import lejos.robotics.RegulatedMotor;
 import lejos.utility.Delay;
 
-public class Grundbetrieb extends Thread {
-	static Sound so1;
-	public final static int[] PIANO = new int[]{4, 25, 500, 7000, 5}; 
+public class Auflegen extends Thread {
+
 	RegulatedMotor m4 = new EV3LargeRegulatedMotor(MotorPort.D);
 	
 	public void run() {
 		Delay.msDelay(5000);
 		while (!Thread.currentThread().isInterrupted()) {
 			while (!Thread.currentThread().isInterrupted()) {
+				//Schieber vorschieben
 				m4.setSpeed(40);
-				 m4.rotate(-85);
-				 Delay.msDelay(150);
-				 m4.rotate(85);
-				 Delay.msDelay(4000);
+				m4.rotate(-85);
+				//Kurz abwarten
+				Delay.msDelay(150);
+				//Schieber zurückschieben
+				m4.rotate(85);
+				//Für 4 Sekunden pausieren
+				Delay.msDelay(4000);
 			 }
 			try {
+				//Für 1 Sekunde pausieren.
 				Thread.sleep(1000);
 				}
 			catch (InterruptedException intExc) {
