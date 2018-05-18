9bffae8fa4ff9a138601f38ed2a1d8a3375c931f
Pascal <Z_pascal@yahoo.de>
2018-05-18 11:57:48 +0200

    - Update the TCP/IP socket connection

--- HSLUColorSorter/src/colorsorter/CommandListener.java
+++ HSLUColorSorter/src/colorsorter/CommandListener.java
@@ -14,53 +14,51 @@ import java.util.ArrayList;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class CommandListener extends Thread {
 
-		public static void main(String argv[]) throws Exception {
+		public static void main(String[] args) throws IOException, ParseException {
+		   // Aufbau eines Loops und Aufrufen der Methode Auslagerung die eine Dauerhafte Socket Verbindung aufbaut
+			while(true) {
+			   auslagerung();
+		   }
+		}
+	
+		public static void auslagerung() throws IOException, ParseException {
+			// Aufbau des Sockets
 			ServerSocket welcomeSocket = new ServerSocket(5555);
-		    System.out.println("Höre auf Befehle...");
+			// Aktzeptieren der Verbindung
 		    Socket connectionSocket = welcomeSocket.accept();
 		    BufferedReader reader = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 	        String clientSentence = reader.readLine();
-	        System.out.println("Client data: "+clientSentence);
 	        connectionSocket.close();
 	        welcomeSocket.close();
-		    //Pascal: JSON hat hier irgendwie noch nicht funktioniert, im Moment reagiert 
-		    //er auf ganz normale Strings, die übermittelt werden.
+	        
 		    JSONParser parser = new JSONParser();
 		    JSONObject obj = (JSONObject) parser.parse(clientSentence);
 		    String commands = (String) obj.get("commands"); 
-		    //String commands = clientSentence;
-		    welcomeSocket.close();
-		    System.out.println("Empfangener Befehl:" +commands);
+		    
 		    if (commands.equals("start")) {
 		    	startev3();
-		    	new CommandListener();
-		    }
-		    else if (commands.equals("stop")) {
+		    }else if (commands.equals("stop")) {
 		    	stopev3();
-		    	new CommandListener();
-		    }
-		    else if (commands.equals("filterspecificcolor")) {
+		    }else if (commands.equals("filterspecificcolor")) {
 		    	String color = (String) obj.get("color");
 		    	filterspecificcolorev3(color);
-		    }
-		    else if (commands.equals("filterspecificcolorandnumber")) {
+		    }else if (commands.equals("filterspecificcolorandnumber")) {
 		    	String color = (String) obj.get("color");
 		    	int number = (int) obj.get("number");
 		    	filterspecificcolorandnumberev3(color,number);
-		    }
-		    else if(commands.equals("filterspecificcolorandnumberlist")) {
+		    }else if(commands.equals("filterspecificcolorandnumberlist")) {
 		    	  // Daten kommen mittels JSON Array an
 		    	   String colorlist = (String) obj.get("colorlist");
 		    	   String numberlist = (String) obj.get("colorlist");
-		    }
-		    else {
+		    }else {
 		    	//nichts
 		    }
 		}
-	    
+		
 	    protected static void startev3() throws IOException {
 	    	Sortierer.machinerunning = true;
 	 	   	Scannen s1 = new Scannen();
